d7941ff02ce8314fd02b7b05f90ef80c71687100
nadavramon <159230565+nadavramon@users.noreply.github.com>
2026-05-24 18:48:36 +0300

    Move BrowserRouter from main.tsx into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 import Layout from './layout/Layout'
 import CounterApp from './counter/CounterApp'
 import TicTacToe from './tic-tac-toe/TicTacToe'
@@ -7,14 +7,16 @@ import NotFoundPage from './pages/NotFoundPage'
 
 export default function App() {
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route index element={<Navigate to="/counters" replace />} />
-                <Route path="counters" element={<CounterApp />} />
-                <Route path="tic-tac-toe" element={<TicTacToe />} />
-                <Route path="todo" element={<TodoPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Route>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route index element={<Navigate to="/counters" replace />} />
+                    <Route path="counters" element={<CounterApp />} />
+                    <Route path="tic-tac-toe" element={<TicTacToe />} />
+                    <Route path="todo" element={<TodoPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
